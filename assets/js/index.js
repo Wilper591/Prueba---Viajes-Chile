b4c35882aad746a280337d7ef7815100c2a9bc8a
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-01-26 14:42:19 -0300

    Añadiendo tooltip con bootstrap al boton enviar de formulario

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+/* Tooltip Bootstrap v5.3 */
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
